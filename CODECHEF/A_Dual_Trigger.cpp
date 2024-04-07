ad837916abc9704fdf4cf79ba57d13ecde9cd3b1
ASH <singhashirwad2003@gmail.com>
2024-04-07 22:15:18 +0530

    updated

---
+++ CODECHEF/A_Dual_Trigger.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        
+        bool possible = true;
+        
+        // Check if there are consecutive '00' in the desired configuration
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == '0' && s[i + 1] == '0') {
+                possible = false;
+                break;
+            }
+        }
+        
+        // Check if the first and last elements are different
+        if (s[0] != s[n - 1]) {
+            possible = true;
+        }
+        
+        // Output the result
+        cout << (possible ? "YES" : "NO") << endl;
+    }
+    
+    return 0;
+}
---
+++ CODEFORCES/A_Team.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+  
+    int implemented_problems = 0;
+
+   
+    for (int i = 0; i < n; ++i) {
+       
+        int p, v, t;
+        cin >> p >> v >> t;
+        
+      
+        int sure_count = p + v + t;
+        
+        
+        if (sure_count >= 2) {
+            implemented_problems++;
+        }
+    }
+
+    
+    cout << implemented_problems << endl;
+
+    return 0;
+}
